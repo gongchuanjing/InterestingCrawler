959ffb050815cb21bbf1a1722d2692d9fa667b57
gcj <longgongsanri@163.com>
2018-10-21 14:43:43 +0800

    爬虫入门，HelloWorld

---
+++ src/main/java/tk/chuanjing/demo/crawler/Crawler01HelloWorld.java
@@ -0,0 +1,36 @@
+package tk.chuanjing.demo.crawler;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @author ChuanJing
+ * @date 2018/10/20 22:12
+ */
+public class Crawler01HelloWorld {
+
+    public static void main(String[] args) throws Exception {
+        //1. 打开浏览器,创建HttpClient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        //2. 输入网址,发起get请求创建HttpGet对象
+        HttpGet httpGet = new HttpGet("http://www.baidu.com");
+
+        //3.按回车，发起请求，返回响应，使用HttpClient对象发起请求
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+
+        //4. 解析响应，获取数据
+        //判断状态码是否是200
+        if(response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity httpEntity = response.getEntity();
+            String content = EntityUtils.toString(httpEntity, "utf8");
+
+            System.out.println(content);
+        }
+    }
+}
